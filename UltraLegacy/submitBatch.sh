10297bdfbf307f96620135b5a23e660e7d205a42
Josh Hiltbrand <hiltbran@umn.edu>
2021-05-21 18:25:31 -0500

    add a little test

--- UltraLegacy/submitBatch.sh
+++ UltraLegacy/submitBatch.sh
@@ -1,106 +1,104 @@
 #! /bin/bash
 
-OUTPUTDIR=$1
-
 ##########
 #  2016  #
 ##########
 
-python submitJob.py --tag 2016_RPV_500  --year 2016 --model RPV --mass 500 --nEvents 126000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_500  --year 2016 --model SYY --mass 500 --nEvents 126000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_500  --year 2016 --model RPV --mass 500 --nEvents 126000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_500  --year 2016 --model SYY --mass 500 --nEvents 126000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_550  --year 2016 --model RPV --mass 550 --nEvents 286000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_550  --year 2016 --model SYY --mass 550 --nEvents 286000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_550  --year 2016 --model RPV --mass 550 --nEvents 286000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_550  --year 2016 --model SYY --mass 550 --nEvents 286000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_600  --year 2016 --model RPV --mass 600 --nEvents 374000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_600  --year 2016 --model SYY --mass 600 --nEvents 374000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_600  --year 2016 --model RPV --mass 600 --nEvents 374000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_600  --year 2016 --model SYY --mass 600 --nEvents 374000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_650  --year 2016 --model RPV --mass 650 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_650  --year 2016 --model SYY --mass 650 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_650  --year 2016 --model RPV --mass 650 --nEvents 412000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_650  --year 2016 --model SYY --mass 650 --nEvents 412000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_700  --year 2016 --model RPV --mass 700 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_700  --year 2016 --model SYY --mass 700 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_700  --year 2016 --model RPV --mass 700 --nEvents 412000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_700  --year 2016 --model SYY --mass 700 --nEvents 412000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_750  --year 2016 --model RPV --mass 750 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_750  --year 2016 --model SYY --mass 750 --nEvents 412000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_750  --year 2016 --model RPV --mass 750 --nEvents 412000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_750  --year 2016 --model SYY --mass 750 --nEvents 412000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_800  --year 2016 --model RPV --mass 800 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_800  --year 2016 --model SYY --mass 800 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_800  --year 2016 --model RPV --mass 800 --nEvents 456000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_800  --year 2016 --model SYY --mass 800 --nEvents 456000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_850  --year 2016 --model RPV --mass 850 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_850  --year 2016 --model SYY --mass 850 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_850  --year 2016 --model RPV --mass 850 --nEvents 456000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_850  --year 2016 --model SYY --mass 850 --nEvents 456000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_900  --year 2016 --model RPV --mass 900 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_SYY_900  --year 2016 --model SYY --mass 900 --nEvents 456000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_900  --year 2016 --model RPV --mass 900 --nEvents 456000 --eventsPerJob 500
+python submitJob.py --tag 2016_SYY_900  --year 2016 --model SYY --mass 900 --nEvents 456000 --eventsPerJob 500
 
 ##########
 #  2017  #
 ##########
 
-python submitJob.py --tag 2017_RPV_500  --year 2017 --model RPV --mass 500 --nEvents  34000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_500  --year 2017 --model SYY --mass 500 --nEvents  34000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_500  --year 2017 --model RPV --mass 500 --nEvents  34000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_500  --year 2017 --model SYY --mass 500 --nEvents  34000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_550  --year 2017 --model RPV --mass 550 --nEvents 234000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_550  --year 2017 --model SYY --mass 550 --nEvents 234000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_550  --year 2017 --model RPV --mass 550 --nEvents 234000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_550  --year 2017 --model SYY --mass 550 --nEvents 234000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_600  --year 2017 --model RPV --mass 600 --nEvents 342000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_600  --year 2017 --model SYY --mass 600 --nEvents 342000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_600  --year 2017 --model RPV --mass 600 --nEvents 342000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_600  --year 2017 --model SYY --mass 600 --nEvents 342000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_650  --year 2017 --model RPV --mass 650 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_650  --year 2017 --model SYY --mass 650 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_650  --year 2017 --model RPV --mass 650 --nEvents 388000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_650  --year 2017 --model SYY --mass 650 --nEvents 388000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_700  --year 2017 --model RPV --mass 700 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_700  --year 2017 --model SYY --mass 700 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_700  --year 2017 --model RPV --mass 700 --nEvents 388000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_700  --year 2017 --model SYY --mass 700 --nEvents 388000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_750  --year 2017 --model RPV --mass 750 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_750  --year 2017 --model SYY --mass 750 --nEvents 388000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_750  --year 2017 --model RPV --mass 750 --nEvents 388000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_750  --year 2017 --model SYY --mass 750 --nEvents 388000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_800  --year 2017 --model RPV --mass 800 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_800  --year 2017 --model SYY --mass 800 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_800  --year 2017 --model RPV --mass 800 --nEvents 444000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_800  --year 2017 --model SYY --mass 800 --nEvents 444000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_850  --year 2017 --model RPV --mass 850 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_850  --year 2017 --model SYY --mass 850 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_850  --year 2017 --model RPV --mass 850 --nEvents 444000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_850  --year 2017 --model SYY --mass 850 --nEvents 444000 --eventsPerJob 500
 
-python submitJob.py --tag 2017_RPV_900  --year 2017 --model RPV --mass 900 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2017_SYY_900  --year 2017 --model SYY --mass 900 --nEvents 444000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2017_RPV_900  --year 2017 --model RPV --mass 900 --nEvents 444000 --eventsPerJob 500
+python submitJob.py --tag 2017_SYY_900  --year 2017 --model SYY --mass 900 --nEvents 444000 --eventsPerJob 500
 
 ##########
 #  2018  #
 ##########
 
-python submitJob.py --tag 2018_RPV_500  --year 2018 --model RPV --mass 500 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_500  --year 2018 --model SYY --mass 500 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_500  --year 2018 --model RPV --mass 500 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_500  --year 2018 --model SYY --mass 500 --nEvents 100000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_550  --year 2018 --model RPV --mass 550 --nEvents 263000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_550  --year 2018 --model SYY --mass 550 --nEvents 263000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_550  --year 2018 --model RPV --mass 550 --nEvents 263000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_550  --year 2018 --model SYY --mass 550 --nEvents 263000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_600  --year 2018 --model RPV --mass 600 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_600  --year 2018 --model SYY --mass 600 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_600  --year 2018 --model RPV --mass 600 --nEvents 332000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_600  --year 2018 --model SYY --mass 600 --nEvents 332000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_650  --year 2018 --model RPV --mass 650 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_650  --year 2018 --model SYY --mass 650 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_650  --year 2018 --model RPV --mass 650 --nEvents 332000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_650  --year 2018 --model SYY --mass 650 --nEvents 332000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_700  --year 2018 --model RPV --mass 700 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_700  --year 2018 --model SYY --mass 700 --nEvents 332000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_700  --year 2018 --model RPV --mass 700 --nEvents 332000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_700  --year 2018 --model SYY --mass 700 --nEvents 332000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_750  --year 2018 --model RPV --mass 750 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_750  --year 2018 --model SYY --mass 750 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_750  --year 2018 --model RPV --mass 750 --nEvents 416000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_750  --year 2018 --model SYY --mass 750 --nEvents 416000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_800  --year 2018 --model RPV --mass 800 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_800  --year 2018 --model SYY --mass 800 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_800  --year 2018 --model RPV --mass 800 --nEvents 416000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_800  --year 2018 --model SYY --mass 800 --nEvents 416000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_850  --year 2018 --model RPV --mass 850 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_850  --year 2018 --model SYY --mass 850 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_850  --year 2018 --model RPV --mass 850 --nEvents 416000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_850  --year 2018 --model SYY --mass 850 --nEvents 416000 --eventsPerJob 500
 
-python submitJob.py --tag 2018_RPV_900  --year 2018 --model RPV --mass 900 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2018_SYY_900  --year 2018 --model SYY --mass 900 --nEvents 416000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2018_RPV_900  --year 2018 --model RPV --mass 900 --nEvents 416000 --eventsPerJob 500
+python submitJob.py --tag 2018_SYY_900  --year 2018 --model SYY --mass 900 --nEvents 416000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_400_chi200  --year 2016 --model RPV200 --mass 400 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_550_chi200  --year 2016 --model RPV200 --mass 550 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_700_chi200  --year 2016 --model RPV200 --mass 700 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_850_chi200  --year 2016 --model RPV200 --mass 850 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_400_chi200  --year 2016 --model RPV200 --mass 400 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_550_chi200  --year 2016 --model RPV200 --mass 550 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_700_chi200  --year 2016 --model RPV200 --mass 700 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_850_chi200  --year 2016 --model RPV200 --mass 850 --nEvents 100000 --eventsPerJob 500
 
-python submitJob.py --tag 2016_RPV_400_chi300  --year 2016 --model RPV300 --mass 400 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_550_chi300  --year 2016 --model RPV300 --mass 550 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_700_chi300  --year 2016 --model RPV300 --mass 700 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
-python submitJob.py --tag 2016_RPV_850_chi300  --year 2016 --model RPV300 --mass 850 --nEvents 100000 --eventsPerJob 500 --outputDir ${OUTPUTDIR}
+python submitJob.py --tag 2016_RPV_400_chi300  --year 2016 --model RPV300 --mass 400 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_550_chi300  --year 2016 --model RPV300 --mass 550 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_700_chi300  --year 2016 --model RPV300 --mass 700 --nEvents 100000 --eventsPerJob 500
+python submitJob.py --tag 2016_RPV_850_chi300  --year 2016 --model RPV300 --mass 850 --nEvents 100000 --eventsPerJob 500
---
+++ UltraLegacy/submitTest.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+##########
+#  2016  #
+##########
+
+python submitJob.py --tag 2016_RPV_500  --year 2016 --model RPV --mass 500 --nEvents 10 --eventsPerJob 10
+
+##########
+#  2017  #
+##########
+
+python submitJob.py --tag 2017_RPV_500  --year 2017 --model RPV --mass 500 --nEvents 10 --eventsPerJob 10
+
+##########
+#  2018  #
+##########
+
+python submitJob.py --tag 2018_RPV_500  --year 2018 --model RPV --mass 500 --nEvents 10 --eventsPerJob 10
